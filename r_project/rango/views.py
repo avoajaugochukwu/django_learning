0b751368adcf3428b4f5bde3bfddf7703afc1347
avoajaugochukwu <karlthegoo@hotmail.com>
2015-02-09 14:21:33 +0100

    added coulumns: likes, views, slug to categories

--- r_project/rango/views.py
+++ r_project/rango/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from rango.models import Category
 # Create your views here.
 
 def index(request):
-	context_dict = {'boldmessage': 'I am the new face of Django'}
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
 	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
