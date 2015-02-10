d9d5b7d900edb46c7115917d9a550cd65139b32a
avoajaugochukwu <avoajaugochukwu@github.com>
2015-02-10 07:53:11 +0100

    db things

--- d_off_tutorial/polls/models.py
+++ d_off_tutorial/polls/models.py
@@ -8,8 +8,14 @@ class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
+	def __unicode__(self):
+		return self.question_text
+
 #Choice database representation
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
+
+	def __unicode__(self):
+		return self.choice_text
