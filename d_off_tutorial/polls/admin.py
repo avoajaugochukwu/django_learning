636b150db8520840892d30a123c963f72c6b7882
avoajaugochukwu <avoajaugochukwu@github.com>
2015-02-11 04:00:20 +0100

    admin representation

--- d_off_tutorial/polls/admin.py
+++ d_off_tutorial/polls/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 
-from polls.models import Question
+from polls.models import Question, Choice
 # Register your models here.
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline): #StackedInline
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+			(None,							 {'fields': {'question_text'}}),
+			('Date information', {'fields': ['pub_date'], 'classes': ['collapse']})
+	]
+
+	inlines = [ChoiceInline]
+
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
+#fields used to reorder form fields
