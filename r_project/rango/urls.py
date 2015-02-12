8c394f2b5778f1d6ed0cc3c070051e283e015754
avoajaugochukwu <karlthegoo@hotmail.com>
2015-02-12 15:23:54 +0100

    forms.py

--- r_project/rango/urls.py
+++ r_project/rango/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
 from rango import views
 # for uploading files
 from django.conf import settings
@@ -6,6 +6,7 @@ from django.conf import settings
 urlpatterns = patterns('',
 		url(r'^$', views.index, name='index'),
 		url(r'^about$', views.about),
+		url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
 	)
 
 if settings.DEBUG:
--- r_project/rango/views.py
+++ r_project/rango/views.py
@@ -1,13 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from rango.models import Category
+from rango.models import Category, Page
 # Create your views here.
 
 def index(request):
+	context_dict = {}
+
 	category_list = Category.objects.order_by('-likes')[:5]
-	context_dict = {'categories': category_list}
+	context_dict['categories'] = category_list
+
+	most_viewed_pages_list = Page.objects.order_by('-views')[:5]
+	context_dict['most_viewed_pages'] = most_viewed_pages_list
+
 	return render(request, 'rango/index.html', context_dict)
+
+
+
+
+def category(request, category_name_slug):
+	context_dict = {}
+
+	try:
+		#get returns model instance(1)
+		category = Category.objects.get(slug=category_name_slug)
+		context_dict['category_name'] = category.name
+
+
+		#get pages associated with category from above
+		#filter can return multiple model instances
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+
+		context_dict['category'] = category
+
+	except Category.DoesNotExist:
+		#The template displays 'no category' message so no logic here
+		pass
+
+	return render(request, 'rango/category.html', context_dict)
+
+
+
 
 def about(request):
 	return HttpResponse('This is the about page')
