beda48526cfdfd2ccfef13e01f69718b85bbcfc1
avoajaugochukwu <avoajaugochukwu@github.com>
2015-02-12 03:21:11 +0100

    fetching with slug

--- r_project/rango/urls.py
+++ r_project/rango/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 urlpatterns = patterns('',
 		url(r'^$', views.index, name='index'),
 		url(r'^about$', views.about),
+		url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
 	)
 
 if settings.DEBUG:
