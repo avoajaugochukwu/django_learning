d006cd2a0fdb2516f74c238adfc3b26d2ee10dee
avoajaugochukwu <avoajaugochukwu@github.com>
2015-02-15 18:34:59 +0100

    add_page

--- r_project/rango/views.py
+++ r_project/rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 from rango.models import Category, Page
 
-from rango.forms import CategoryForm
+from rango.forms import CategoryForm, PageForm
 # Create your views here.
 
 def index(request):
@@ -66,3 +66,28 @@ def add_category(request):
 		form = CategoryForm()
 
 	return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+	try:
+		cat = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		cat = None
+
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if cat:
+				page = form.save(commit=False)
+				page.category = cat
+				page.views = 0
+				page.save()
+
+				return category(request, category_name_slug)
+		else:
+			print form.errors
+	else:
+		form = PageForm()
+
+	context_dict = {'form': form, 'category': cat}
+	
+	return render(request, 'rango/add_page.html', context_dict)
